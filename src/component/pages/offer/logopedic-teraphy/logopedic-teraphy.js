126fbad9afece703d4dcfd77628109f751a9979f
Rafał Prokopiak <rafal.prokopiak@ttpsc.pl>
2021-09-24 13:38:17 +0200

    responsive logopedic-teraphy-site

--- src/component/pages/offer/logopedic-teraphy/logopedic-teraphy.js
+++ src/component/pages/offer/logopedic-teraphy/logopedic-teraphy.js
@@ -2,12 +2,25 @@ import React from 'react'
 import './logopedic-teraphy.css'
 import LTCover from './photos/Terapia_logopedyczna_zakladka_kangurek.jpg';
 import OfferTemplate from '../offer-template/offer-template';
+import teraphy from './photos/Terapia_logopedyczna.jpg'
 
 export default class LogopedicTeraphy extends React.Component {
     render() {
         return (
-            <OfferTemplate header="TERAPIA LOGOPEDYCZNA" img={LTCover}>
-                <h1>coś</h1>
+            <OfferTemplate header="TERAPIA LOGOPEDYCZNA" img={LTCover} name="terapia-logopedyczna" margin={0}>
+                <div>
+                    <p><b>Terapia logopedyczna:</b></p>
+                    <p>Wczesna diagnoza problemów logopedycznych pozwala w porę dostrzec trudności dziecka i szybciej oraz skuteczniej je zniwelować zanim dziecko rozpocznie naukę w szkole.</p>
+                    <ul>
+                        <li>kształtuje prawidłową mowę , dba o jej prawidłowy i jak najlepszy rozwój pod względem fonetycznym, gramatycznym i leksykalnym</li>
+                        <li>doskonali mowę już ukształtowaną</li>
+                        <li>usuwa wady mowy</li>
+                        <li>naucza mowy (mówienia i rozumienia) w wypadku jej braku lub utraty</li>
+                        <li>usuwa zaburzenia głosu</li>
+                        <li>usuwanie trudności w czytaniu i pisaniu</li>
+                    </ul>
+                </div>
+                <img src={teraphy} alt="terapia logopedyczna" />
             </OfferTemplate>
         )
     }
