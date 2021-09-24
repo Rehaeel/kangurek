4aaa6448310d7b2bd59cbdf05694c205bf38462e
Rafał Prokopiak <rafal.prokopiak@ttpsc.pl>
2021-09-24 20:43:14 +0200

    deleting out-dated terapist and adding Basia

--- src/component/pages/terapists/terapeuci-list.js
+++ src/component/pages/terapists/terapeuci-list.js
@@ -1,21 +1,28 @@
-import Julita from './photos/Julita.jpg';
 import Beata from './photos/Beata-Cholewczynska.jpg';
-import Katarzyna from './photos/Katarzyna-Palgan.jpg';
-import Malgorzata from './photos/Małgorzata-Soszka-Szerul.jpg';
-import Monika from './photos/Monika-Marczak.jpg';
+import Basia from './photos/Barbara-Prokopiak.jpg'
 
 export const terapeuciList = [
     {
-        name: 'Julita Jarecka',
-        image: Julita,
+        name: 'Barbara Prokopiak',
+        image: Basia,
         education: [
-            'FIZJOTERAPEUTA',
-            'PEDAGOG O SPECJALIZACJI OPIEKA I WYCHOWANIE Z PROFILAKTYKĄ SPOŁECZNĄ',
-            'TERAPEUTA PNF ( TOROWANIA NERWOWO-MIĘŚNIOWEGO )',
-            'TERAPEUTA DZIECI Z ZABURZENIAMI DYSPRAKTYCZNYMI ( DCD )'
+            'OLIGOFRENOPEDAGOG',
+            'TERAPEUTA INTEGRACJI SENSORYCZNEJ',
+            'TERAPEUTA RĘKI',
+            'TERAPEUTA WCZESNEJ INTERWENCJI I WCZESNEGO WSPOMAGANIA ROZWOJU DZIECKA',
+            'DOGOTERAPEUTA',
+            'TERAPEUTA TOMATISA',
+            'PEDAGOG M.MONTESSORI',
+            'TERAPEUTA SENSOMOTORYCZNEJ TERAPII WIDZENIA'
         ],
-        expirience: `Z wykształcenia i zamiłowania fizjoterapeuta oraz pedagog opiekuńczo-wychowawczy. Posiada doświadczenie w  pracy z dziećmi z  wadami postawy, rozwojowymi zaburzeniami koordynacji (DCD) i po urazach narządu ruchu. Ukończyła kurs PNF podstawowy, DCD „Children Developmental Dyspraxia” według założeń metody NDT Bobath oraz Taping Rehabilitacyjny. Posiada również certyfikat ukończenia szkolenia obejmującego pracę z dziećmi metodą Ruchu Rozwijającego Weroniki Sherborne. Jako fizjoterapeuta praktykowała w sanatorium w Inowrocławiu, w Klinice Zdrówko w Niemczu, na oddziałach: Neurochirurgii, Ortopedii i Traumatologii Narządu Ruchu, Rehabilitacji oraz pracowni kinezyterapii i fizykoterapii w Szpitalu Uniwersyteckim im. Jurasza w Bydgoszczy oraz przychodni Reumatologiczno-Rehabilitacyjnej REH-MED w Bydgoszczy.${<br />} Zaprasza do współpracy wszystkich rodziców zaniepokojonych postawą ciała oraz  funkcjonowaniem ruchowym swoich dzieci.`
-    }, {
+        expirience: <>
+            <p>Z wyksztalcenia pedagog nauczania początkowego i wychowania fizycznego. Ma wieloletnie  doświadczenie w pracy z dziećmi zarówno w normie jak i z różnym stopniem niepełnosprawności. Dobry kontakt z dziećmi autystycznymi i niemówiącymi.</p>
+            <p>Od 1998 roku pracuje jako nauczyciel  w grupie rewalidacyjno-wychowawczej oraz  w klasie z   uczniami z autyzmem w Ośrodku Szkolno Wychowawczym nr 3 w Bydgoszczy. Od 2013 roku prowadzi terapię integracji sensorycznej w Centrum Wspierania Rozwoju SENSO w Osielsku oraz w Centrum Terapii i Rehabilitacji SENSIS w Bydgoszczy.</p>
+            <p>Nauczyciel akademicki z zakresu <em>Komunikacji alternatywnej i wspomagającej</em> oraz <em>Zaburzeń integracji sensorycznej</em>. Dzieli się swoją wiedzą ze studentami Bydgoskiego Zakładu Doskonalenia Zawodowego, K-PSW, RCE w Bydgoszczy oraz w Pile na WSG i Pracowni Edukacji i Coachingu.</p>
+            <p>W-ce prezes stowarzyszenia Ruch na Rzecz Godności Osób Niepełnosprawnych WYZWANIE.</p>
+        </>
+    },
+    {
         name: 'Beata Cholewczyńska',
         image: Beata,
         education: [
@@ -25,35 +32,5 @@ export const terapeuciList = [
             'TERAPEUTA RĘKI'
         ],
         expirience: `Od 1995 roku nauczycielka w Ośrodku Szkolno-Wychowawczym nr 3 w Bydgoszczy. Specjalista w zakresie nauki czytania i pisania, prowadzonych według programu Wesołe literki- uczymy się czytać oraz Umiem liczyć – rozwijamy myślenie matematyczne w grach i zabawach. Kreatywna w  pracy z dziećmi z różnymi niepełnosprawnościami. Współautor „Programu zintegrowanej edukacji wczesnoszkolnej w klasach I – III szkoły podstawowej dla dzieci upośledzonych umysłowo w stopniu lekkim” dopuszczonego do użytku szkolnego przez Ministra Edukacji Narodowej, nr DKW-4014-60/00. Pracowała jako wykładowca akademicki z zakresu metodyki nauczania dziecka upośledzonego umysłowo w stopniu lekkim w szkole podstawowej oraz  z zakresu rewalidacji dziecka upośledzonego umysłowo na Wyższej Szkole Zarządzania i Finansów oraz na Kujawsko-Pomorskiej Szkole Wyższej. Współpracowała z KP CEN prowadząc zajęcia pokazowe lekcji metodą ośrodków pracy. Ciągle poszukuje i rozwija swój warsztat pracy jako terapeuta integracji sensorycznej.`
-    }, {
-        name: 'Monika Marczak',
-        image: Monika,
-        education: [
-            'OLIGOFRENOPEDAGOG',
-            'TERAPEUTA INTEGRACJI SENSORYCZNEJ',
-            'TERAPEUTA RĘKI',
-            'TERAPEUTA WCZESNEGO WSPOMAGANIA ROZWOJU DZIECKA',
-            'TRENER Sensoplastyka®',
-            'PEDAGOG M.MONTESSORI'
-        ],
-        expirience: `Z wyksztalcenia  nauczycielka edukacji przedszkolnej i wczesnoszkolnej.  Swoje doświadczenie zawodowe zdobyła pracując z dziećmi i młodzieżą niepełnosprawną intelektualnie i sprzężoną, z opóźnionym rozwojem mowy w Ośrodku Szkolno – Wychowawczym nr 3 w Bydgoszczy oraz  jako terapeuta wczesnego wspomagania w Poradni Psychologiczno- Pedagogicznej SENSO w Osielsku. Specjalistka i miłośnik  komunikowania się z dziećmi niemówiącymi metodą MAKATON. Współorganizator zajęć dla Młodych Sportowców w ramach Olimpiad Specjalnych dla dzieci z niepełnosprawnością intelektualną w wieku 2-7 lat  Członek zarządu stowarzyszenia Ruch na Rzecz Godności Osób Niepełnosprawnych WYZWANIE.`
-    }, {
-        name: 'Małgorzata Soszka-Szerul',
-        image: Malgorzata,
-        education: [
-            'LOGOPEDA',
-            'NEUROLOGOPEDA'
-        ],
-        expirience: `Z wykształcenia pedagog nauczania zintegrowanego i terapii pedagogicznej. Swoje doświadczenie zdobyła pracując zarówno z dziećmi, jak i osobami dorosłymi. Specjalizuje się w pracy z osobami chorymi i niepełnosprawnymi. W procesie terapii dostosowuje metody i techniki do indywidualnych potrzeb oraz możliwości pacjenta.`
-    }, {
-        name: 'Katarzyna Pałgan',
-        image: Katarzyna,
-        education: [
-            'OLIGOFRENOPEDAGOG',
-            'TERAPEUTA INTEGRACJI SENSORYCZNEJ',
-            'TERAPEUTA RĘKI',
-            'PEDAGOG MONTESSORI'
-        ],
-        expirience: `Z wykształcenia  pedagog  wczesnoszkolny. Od 2012 roku pracuje z dziećmi z różnym stopniem niepełnosprawności i sprzężeniami  w Ośrodku Szkolno – Wychowawczym nr 3 w Bydgoszczy.  Specjalizuje się w porozumiewaniu  z dziećmi z problemami komunikacyjnymi, prowadząc zajęcia metodą MAKATON. W trakcie studiów pracowała jako nauczyciel w Zespole Szkół Specjalnych nr 11 w Krakowie.   Systematycznie podnosi swoje umiejętności m.in. na terapii pedagogicznej z rewalidacją indywidualną oraz licznych kursach w zakresie integracji sensorycznej.`
     }
 ]
