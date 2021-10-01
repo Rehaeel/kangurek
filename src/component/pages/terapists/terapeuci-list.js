626610e6c7b35dc576ed6aa1b0b270a5154e3869
ProkopiakR <rafal.prokopiak@ttpsc.pl>
2021-10-01 10:38:59 +0200

    adding Dominika into terapists

--- src/component/pages/terapists/terapeuci-list.js
+++ src/component/pages/terapists/terapeuci-list.js
@@ -1,90 +1,133 @@
-import Beata from './photos/Beata-Cholewczynska.jpg';
-import Basia from './photos/Barbara-Prokopiak.jpg';
-import Marysia from './photos/Maria-Bandoch.jpg';
-import Gosia from './photos/Malgorzata-Lubiszewska.jpg';
-import Ala from './photos/Alicja-Wenda.jpg';
-import Wika from './photos/Wiktoria-Kendra.jpg';
-import Monia from './photos/Monika-Frontczak.jpg';
+import Beata from "./photos/Beata-Cholewczynska.jpg";
+import Basia from "./photos/Barbara-Prokopiak.jpg";
+import Marysia from "./photos/Maria-Bandoch.jpg";
+import Gosia from "./photos/Malgorzata-Lubiszewska.jpg";
+import Ala from "./photos/Alicja-Wenda.jpg";
+import Wika from "./photos/Wiktoria-Kendra.jpg";
+import Monia from "./photos/Monika-Frontczak.jpg";
+import Domi from "./photos/Dominika-Kondek.jpg";
 
 export const terapeuciList = [
     {
-        name: 'Barbara Prokopiak',
+        name: "Barbara Prokopiak",
         image: Basia,
         education: [
-            'OLIGOFRENOPEDAGOG',
-            'TERAPEUTA INTEGRACJI SENSORYCZNEJ',
-            'TERAPEUTA RĘKI',
-            'TERAPEUTA WCZESNEJ INTERWENCJI I WCZESNEGO WSPOMAGANIA ROZWOJU DZIECKA',
-            'DOGOTERAPEUTA',
-            'TERAPEUTA TOMATISA',
-            'PEDAGOG M.MONTESSORI',
-            'TERAPEUTA SENSOMOTORYCZNEJ TERAPII WIDZENIA'
+            "OLIGOFRENOPEDAGOG",
+            "TERAPEUTA INTEGRACJI SENSORYCZNEJ",
+            "TERAPEUTA RĘKI",
+            "TERAPEUTA WCZESNEJ INTERWENCJI I WCZESNEGO WSPOMAGANIA ROZWOJU DZIECKA",
+            "DOGOTERAPEUTA",
+            "TERAPEUTA TOMATISA",
+            "PEDAGOG M.MONTESSORI",
+            "TERAPEUTA SENSOMOTORYCZNEJ TERAPII WIDZENIA",
         ],
-        expirience: <>
-            <p>Z wyksztalcenia pedagog nauczania początkowego i wychowania fizycznego. Ma wieloletnie  doświadczenie w pracy z dziećmi zarówno w normie jak i z różnym stopniem niepełnosprawności. Dobry kontakt z dziećmi autystycznymi i niemówiącymi.</p>
-            <p>Od 1998 roku pracuje jako nauczyciel  w grupie rewalidacyjno-wychowawczej oraz  w klasie z   uczniami z autyzmem w Ośrodku Szkolno Wychowawczym nr 3 w Bydgoszczy. Od 2013 roku prowadzi terapię integracji sensorycznej w Centrum Wspierania Rozwoju SENSO w Osielsku oraz w Centrum Terapii i Rehabilitacji SENSIS w Bydgoszczy.</p>
-            <p>Nauczyciel akademicki z zakresu <em>Komunikacji alternatywnej i wspomagającej</em> oraz <em>Zaburzeń integracji sensorycznej</em>. Dzieli się swoją wiedzą ze studentami Bydgoskiego Zakładu Doskonalenia Zawodowego, K-PSW, RCE w Bydgoszczy oraz w Pile na WSG i Pracowni Edukacji i Coachingu.</p>
-            <p>W-ce prezes stowarzyszenia Ruch na Rzecz Godności Osób Niepełnosprawnych WYZWANIE.</p>
-        </>
+        expirience: (
+            <>
+                <p>
+                    Z wyksztalcenia pedagog nauczania początkowego i wychowania
+                    fizycznego. Ma wieloletnie doświadczenie w pracy z dziećmi
+                    zarówno w normie jak i z różnym stopniem niepełnosprawności.
+                    Dobry kontakt z dziećmi autystycznymi i niemówiącymi.
+                </p>
+                <p>
+                    Od 1998 roku pracuje jako nauczyciel w grupie
+                    rewalidacyjno-wychowawczej oraz w klasie z uczniami z
+                    autyzmem w Ośrodku Szkolno Wychowawczym nr 3 w Bydgoszczy.
+                    Od 2013 roku prowadzi terapię integracji sensorycznej w
+                    Centrum Wspierania Rozwoju SENSO w Osielsku oraz w Centrum
+                    Terapii i Rehabilitacji SENSIS w Bydgoszczy.
+                </p>
+                <p>
+                    Nauczyciel akademicki z zakresu{" "}
+                    <em>Komunikacji alternatywnej i wspomagającej</em> oraz{" "}
+                    <em>Zaburzeń integracji sensorycznej</em>. Dzieli się swoją
+                    wiedzą ze studentami Bydgoskiego Zakładu Doskonalenia
+                    Zawodowego, K-PSW, RCE w Bydgoszczy oraz w Pile na WSG i
+                    Pracowni Edukacji i Coachingu.
+                </p>
+                <p>
+                    W-ce prezes stowarzyszenia Ruch na Rzecz Godności Osób
+                    Niepełnosprawnych WYZWANIE.
+                </p>
+            </>
+        ),
     },
     {
-        name: 'Maria Bandoch',
+        name: "Maria Bandoch",
         image: Marysia,
         education: [
-            'LOGOPEDA',
-            'TERAPEUTA  METODY  MIOFUNKCJONALNEJ',
-            'PEDAGOG PRZEDSZKOLNY',
-            'NIEDŁUGO NEUROLOGOPEDA'
+            "LOGOPEDA",
+            "TERAPEUTA  METODY  MIOFUNKCJONALNEJ",
+            "PEDAGOG PRZEDSZKOLNY",
+            "NIEDŁUGO NEUROLOGOPEDA",
         ],
-        expirience: <>
-            <p>Od zawsze pracuje z dziećmi, nawet bardzo małymi. Z powodzeniem prowadzi też terapię z dorosłymi.</p>
-            <p>Od 2018  logopeda w Centrum Kangurek oraz Poradni Psychologiczno- Pedagogicznej. Bierze udział  w projekcie rządowym „Za życiem” pracuje z małymi dziećmi zagrożony niepełnosprawnością.</p>
-            <p>2017 pracowała w przedszkolu Motylek jako terapeuta behawioralny.</p>
-            <p>Obecnie studiuje neurologopedię we Wrocławiu.</p>
-        </>
+        expirience: (
+            <>
+                <p>
+                    Od zawsze pracuje z dziećmi, nawet bardzo małymi. Z
+                    powodzeniem prowadzi też terapię z dorosłymi.
+                </p>
+                <p>
+                    Od 2018 logopeda w Centrum Kangurek oraz Poradni
+                    Psychologiczno- Pedagogicznej. Bierze udział w projekcie
+                    rządowym „Za życiem” pracuje z małymi dziećmi zagrożony
+                    niepełnosprawnością.
+                </p>
+                <p>
+                    2017 pracowała w przedszkolu Motylek jako terapeuta
+                    behawioralny.
+                </p>
+                <p>Obecnie studiuje neurologopedię we Wrocławiu.</p>
+            </>
+        ),
     },
     {
-        name: 'Beata Cholewczyńska',
+        name: "Beata Cholewczyńska",
         image: Beata,
         education: [
-            'PEDAGOG WYCHOWANIA PRZEDSZKOLNEGO I NAUCZANIA POCZĄTKOWEGO',
-            'OLIGOFRENOPEDAGOG',
-            'TERAPEUTA INTEGRACJI SENSORYCZNEJ',
-            'TERAPEUTA RĘKI'
+            "PEDAGOG WYCHOWANIA PRZEDSZKOLNEGO I NAUCZANIA POCZĄTKOWEGO",
+            "OLIGOFRENOPEDAGOG",
+            "TERAPEUTA INTEGRACJI SENSORYCZNEJ",
+            "TERAPEUTA RĘKI",
         ],
-        expirience: `Od 1995 roku nauczycielka w Ośrodku Szkolno-Wychowawczym nr 3 w Bydgoszczy. Specjalista w zakresie nauki czytania i pisania, prowadzonych według programu Wesołe literki- uczymy się czytać oraz Umiem liczyć – rozwijamy myślenie matematyczne w grach i zabawach. Kreatywna w  pracy z dziećmi z różnymi niepełnosprawnościami. Współautor „Programu zintegrowanej edukacji wczesnoszkolnej w klasach I – III szkoły podstawowej dla dzieci upośledzonych umysłowo w stopniu lekkim” dopuszczonego do użytku szkolnego przez Ministra Edukacji Narodowej, nr DKW-4014-60/00. Pracowała jako wykładowca akademicki z zakresu metodyki nauczania dziecka upośledzonego umysłowo w stopniu lekkim w szkole podstawowej oraz  z zakresu rewalidacji dziecka upośledzonego umysłowo na Wyższej Szkole Zarządzania i Finansów oraz na Kujawsko-Pomorskiej Szkole Wyższej. Współpracowała z KP CEN prowadząc zajęcia pokazowe lekcji metodą ośrodków pracy. Ciągle poszukuje i rozwija swój warsztat pracy jako terapeuta integracji sensorycznej.`
+        expirience: `Od 1995 roku nauczycielka w Ośrodku Szkolno-Wychowawczym nr 3 w Bydgoszczy. Specjalista w zakresie nauki czytania i pisania, prowadzonych według programu Wesołe literki- uczymy się czytać oraz Umiem liczyć – rozwijamy myślenie matematyczne w grach i zabawach. Kreatywna w  pracy z dziećmi z różnymi niepełnosprawnościami. Współautor „Programu zintegrowanej edukacji wczesnoszkolnej w klasach I – III szkoły podstawowej dla dzieci upośledzonych umysłowo w stopniu lekkim” dopuszczonego do użytku szkolnego przez Ministra Edukacji Narodowej, nr DKW-4014-60/00. Pracowała jako wykładowca akademicki z zakresu metodyki nauczania dziecka upośledzonego umysłowo w stopniu lekkim w szkole podstawowej oraz  z zakresu rewalidacji dziecka upośledzonego umysłowo na Wyższej Szkole Zarządzania i Finansów oraz na Kujawsko-Pomorskiej Szkole Wyższej. Współpracowała z KP CEN prowadząc zajęcia pokazowe lekcji metodą ośrodków pracy. Ciągle poszukuje i rozwija swój warsztat pracy jako terapeuta integracji sensorycznej.`,
     },
     {
-        name: 'Małgorzata Lubiszewska',
+        name: "Małgorzata Lubiszewska",
         image: Gosia,
-        education: [
-            'OLIGOFRENOPEDAGOG',
-            'PEDAGOG WYCHOWANIA MUZYCZNEGO'
-        ],
-        expirience: 'Pracuje obecnie w SOSW 3'
+        education: ["OLIGOFRENOPEDAGOG", "PEDAGOG WYCHOWANIA MUZYCZNEGO"],
+        expirience: "Pracuje obecnie w SOSW 3",
     },
     {
-        name: 'Alicja Wenda',
+        name: "Alicja Wenda",
         image: Ala,
-        education: [
-            'DOGOTERAPEUTA'
-        ],
-        expirience: 'Dogoterapią zajmuje się od 13 lat. Satysfakcje daje jej praca z dziećmi i psami. Dogoterapia to idealne połączenie tych dwóch pasji. Wraz z psem Albertem należy do fundacji dogoterapeutycznej "Cane pro hubamo". Obecnie jest w trakcie studiów na kierunku "Pedagogika przedszkolna i wczesnoszkolna". Specjalista do wspomagania terapii przy udziale psa.'
+        education: ["DOGOTERAPEUTA"],
+        expirience:
+            'Dogoterapią zajmuje się od 13 lat. Satysfakcje daje jej praca z dziećmi i psami. Dogoterapia to idealne połączenie tych dwóch pasji. Wraz z psem Albertem należy do fundacji dogoterapeutycznej "Cane pro hubamo". Obecnie jest w trakcie studiów na kierunku "Pedagogika przedszkolna i wczesnoszkolna". Specjalista do wspomagania terapii przy udziale psa.',
     },
     {
-        name: 'Wiktoria Kendra',
+        name: "Wiktoria Kendra",
         image: Wika,
-        education: [
-            'LOGOPEDA'
-        ],
-        expirience: 'Studiuje na studiach II stopnia Pedagogikę o specjalności Logopedia. Dopiero zaczyna swoją drogę zawodową, ciągle poszerza wiedzę  o nowe szkolenia i kursy. W 2020 prowadziła zajęcia profilaktyczne w żłobku. Od 2021 roku pracuje jako logopeda w Centrum „Kangurek” oraz w Gabinetach Terapeutycznych w Nakle nad Notecią. W wolnym czasie prowadzi swoją stronę logopedyczną, za pomocą której chce szerzyć wiedzę logopedyczną. Obecnie kończy 3 poziom Polskiego Języka Migowego w Biurze Wsparcia Niesłyszących- KODA.'
+        education: ["LOGOPEDA"],
+        expirience:
+            "Studiuje na studiach II stopnia Pedagogikę o specjalności Logopedia. Dopiero zaczyna swoją drogę zawodową, ciągle poszerza wiedzę  o nowe szkolenia i kursy. W 2020 prowadziła zajęcia profilaktyczne w żłobku. Od 2021 roku pracuje jako logopeda w Centrum „Kangurek” oraz w Gabinetach Terapeutycznych w Nakle nad Notecią. W wolnym czasie prowadzi swoją stronę logopedyczną, za pomocą której chce szerzyć wiedzę logopedyczną. Obecnie kończy 3 poziom Polskiego Języka Migowego w Biurze Wsparcia Niesłyszących- KODA.",
     },
     {
-        name: 'Monika Frontczak',
+        name: "Monika Frontczak",
         image: Monia,
+        education: ["FIZJOTERAPEUTKA"],
+        expirience:
+            'Mgr. fizjoterapii, pracująca w klinice rehabilitacyjnej z pacjentami dorosłymi oraz dziećmi z wadami postawy. Od 2020 roku pracuje również w Centrum "Kangurek", prowadzi zajęcia ogólnorozwojowe, rehabilitację ruchową, terapię wad postawy',
+    },
+    {
+        name: "Dominika Kondek",
+        image: Domi,
         education: [
-            "FIZJOTERAPEUTKA",
+            "PEDAGOG WYCHOWANIA PRZEDSZKOLNEGO I NAUCZANIA POCZĄTKOWEGO",
+            "TERAPEUTA INTEGRACJI SENSORYCZNEJ",
+            "ANIMATOR CZASU WOLNEGO",
         ],
-        expirience: 'Mgr. fizjoterapii, pracująca w klinice rehabilitacyjnej z pacjentami dorosłymi oraz dziećmi z wadami postawy. Od 2020 roku pracuje również w Centrum "Kangurek", prowadzi zajęcia ogólnorozwojowe, rehabilitację ruchową, terapię wad postawy'
-    }
-]
+        expirience:
+            "Z wykształcenia  nauczycielka edukacji przedszkolnej i wczesnoszkolnej.  Swoje doświadczenie zawodowe zdobyła pracując z dziećmi i młodzieżą jako animator czasu wolnego. Od 2019 roku nauczycielka grupy przedszkolnej, od 2021 terapeuta integracji sensorycznej. Systematycznie pracuje nad poszerzaniem swoich zdolności i umiejętności w trakcie pracy z dziećmi na terapii pedagogicznej połączonej z kreatywnością oraz kursów związanych z integracją sensoryczną i sensoplastyką.",
+    },
+];
