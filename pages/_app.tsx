25b69fe921d164612861dc10c10b60797b16ede7
Rafał Prokopiak <88146956+Rehaeel@users.noreply.github.com>
2022-07-06 13:18:57 +0200

    add favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,10 +3,14 @@ import '@fontsource/lora';
 import '@fontsource/lato';
 import type { AppProps } from 'next/app';
 import Layer from '../components/layer';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }: AppProps) {
 	return (
 		<Layer>
+			<Head>
+				<link rel='shortcut icon' href='/images/logo.svg' />
+			</Head>
 			<Component {...pageProps} />
 		</Layer>
 	);
