adcfadc8c82ec4d30d69e5694644b598ee9861e9
Rafał Prokopiak <88146956+Rehaeel@users.noreply.github.com>
2022-07-06 18:46:45 +0200

    meta viewport

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -10,6 +10,9 @@ function MyApp({ Component, pageProps }: AppProps) {
 		<Layer>
 			<Head>
 				<link rel='shortcut icon' href='/images/logo.svg' />
+				<meta
+					name='viewport'
+					content='width=device-width, initial-scale=1.0'></meta>
 			</Head>
 			<Component {...pageProps} />
 		</Layer>
