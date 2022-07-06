55151405cbf774d87f9cc910d02d22fe7a1c1e4d
Rafał Prokopiak <88146956+Rehaeel@users.noreply.github.com>
2022-07-06 17:13:43 +0200

    menu fix

--- components/menu/index.tsx
+++ components/menu/index.tsx
@@ -16,7 +16,7 @@ const Menu: NextPage = () => {
 		<menu
 			className={`${
 				isOpen ? 'fixed max-h-screen' : 'sticky max-h-12'
-			} transition-max-h top-0 z-50 flex h-screen w-full items-center bg-orange-color pt-12 duration-500 md:pt-0 lg:px-[10%]`}>
+			} transition-max-h top-0 z-50 flex h-screen w-full items-center bg-orange-color pt-12 duration-500 md:max-h-12 md:pt-0 lg:px-[10%]`}>
 			<Link href='/'>
 				<a
 					onClick={() => setIsOpen(false)}
@@ -42,7 +42,7 @@ const Menu: NextPage = () => {
 			<nav
 				className={`${
 					isOpen ? 'left-0 opacity-100' : '-left-full opacity-0'
-				} relative top-0 h-full w-full flex-row overflow-y-auto border-t-2 border-t-orange-darker-color bg-orange-color pb-32 text-white transition-all duration-1000 md:flex md:h-full md:items-center md:justify-end md:bg-transparent md:pb-0`}>
+				} relative top-0 h-full w-full flex-row overflow-y-auto border-t-2 border-t-orange-darker-color bg-orange-color pb-32 text-white transition-all duration-1000 md:left-0 md:flex md:h-full md:items-center md:justify-end md:bg-transparent md:pb-0 md:opacity-100`}>
 				<Link
 					href='https://www.facebook.com/centrumkangurek/'
 					prefetch={false}>
