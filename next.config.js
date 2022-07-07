96c92017bc474acaba24e923c0472151ad8a6ece
Rafał Prokopiak <88146956+Rehaeel@users.noreply.github.com>
2022-07-07 16:42:57 +0200

    phase env

--- next.config.js
+++ next.config.js
@@ -1,6 +1,31 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+const {
+	PHASE_DEVELOPMENT_SERVER,
+	PHASE_PRODUCTION_BUILD,
+	PHASE_PRODUCTION_SERVER,
+	PHASE_EXPORT,
+} = require('next/constants');
 
-module.exports = nextConfig
+/** @type {import('next').NextConfig} */
+
+module.exports = (phase) => {
+	if (
+		phase === PHASE_PRODUCTION_BUILD ||
+		PHASE_PRODUCTION_SERVER ||
+		PHASE_DEVELOPMENT_SERVER
+	) {
+		return {
+			reactStrictMode: true,
+			env: {
+				phase: 'build',
+			},
+		};
+	} else if (phase === PHASE_EXPORT) {
+		return {
+			reactStrictMode: true,
+			env: {
+				phase: 'export',
+			},
+		};
+	}
+	return { reactStrictMode: true };
+};
