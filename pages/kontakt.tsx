cb251b544febc25de2682e618216344755970f39
Rafał Prokopiak <88146956+Rehaeel@users.noreply.github.com>
2022-07-08 11:57:32 +0200

    OG meta - kontakt

--- pages/kontakt.tsx
+++ pages/kontakt.tsx
@@ -3,19 +3,30 @@ import PageContent from '../components/universal/page-content';
 import Kontakt from '../components/pages/kontakt/kontakt';
 import { NextPage } from 'next';
 import Head from 'next/head';
+import { DOMAIN_NAME } from '../utils/constants';
+import { useRouter } from 'next/router';
+
+const title: string = 'Kontakt';
+const description: string =
+	'Zapraszamy do kontaktu z Centrum Terapii Rozwoju Dziecka Kangurek (Centrum-Kangurek)';
 
 const KontaktPage: NextPage = () => {
+	const router = useRouter();
+
 	return (
 		<>
 			<Head>
-				<title>Kontakt</title>
-				<meta
-					name='description'
-					content='Zapraszamy do kontaktu z Centrum Terapii Rozwoju Dziecka Kangurek (Centrum-Kangurek)'
-				/>
+				<title>{title}</title>
+				<meta name='description' content={description} />
 				<meta
 					name='keywords'
 					content='kontakt, integracja sensoryczna, centrum-kangurek'
+				/>
+				<meta property='og:title' content={title} />
+				<meta property='og:description' content={description} />
+				<meta
+					property='og:url'
+					content={`${DOMAIN_NAME}/${router.pathname}`}
 				/>
 			</Head>
 
