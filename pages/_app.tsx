f8c1c176af8d4a28d4d2b363ef5068a9406dc7a6
Rafał Prokopiak <88146956+Rehaeel@users.noreply.github.com>
2023-09-07 14:02:58 +0200

    hide fb id

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -29,11 +29,11 @@ function MyApp({ Component, pageProps }: AppProps) {
                   t.src=v;s=b.getElementsByTagName(e)[0];
                   s.parentNode.insertBefore(t,s)}(window, document,'script',
               'https://connect.facebook.net/en_US/fbevents.js');
-              fbq('init', '617685440434672');
+              fbq('init', `${process.env.NEXT_PUBLIC_FACEBOOK_ID}`);
               fbq('track', 'PageView');
           </script>
           <noscript><img height="1" width="1" style="display:none"
-                         src="https://www.facebook.com/tr?id=617685440434672&ev=PageView&noscript=1"
+                         src=`https://www.facebook.com/tr?id=${process.env.NEXT_PUBLIC_FACEBOOK_ID}&ev=PageView&noscript=1`
           /></noscript>
           <!-- End Meta Pixel Code -->
       </Head>
