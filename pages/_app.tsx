aeaff65ab7453b5a94a4478c2e92d8a4c10482da
Rafał Prokopiak <88146956+Rehaeel@users.noreply.github.com>
2023-09-07 14:16:56 +0200

    place fb code before the return

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,7 +6,38 @@ import Layer from "../components/layer";
 import Head from "next/head";
 import Script from "next/script";
 
+declare global {
+    interface Window {
+        fbq: (a:any, b:any) => void;
+    }
+}
+
 function MyApp({ Component, pageProps }: AppProps) {
+
+    // @ts-ignore
+    (function(f:any,b:any,e:any,v:any,n:any,t:any,s:any) {
+        if(f.fbq)return;
+        n=f.fbq = function(){
+            n.callMethod ?
+            n.callMethod.apply(n,arguments) : n.queue.push(arguments)
+        };
+
+        if(!f._fbq) f._fbq=n;
+
+        n.push=n;
+        n.loaded=!0;
+        n.version='2.0';
+        n.queue=[];
+        t=b.createElement(e);
+        t.async=!0;
+        t.src=v;
+        s=b.getElementsByTagName(e)[0];
+        s.parentNode.insertBefore(t,s)
+    })(window, document,'script', 'https://connect.facebook.net/en_US/fbevents.js');
+
+    window.fbq('init', `${process.env.NEXT_PUBLIC_FACEBOOK_API}`);
+    window.fbq('track', 'PageView');
+
   return (
     <Layer>
       <Head>
@@ -18,24 +49,6 @@ function MyApp({ Component, pageProps }: AppProps) {
           content="https://kangurek2.vercel.app/images/logo.png"
         />
         <meta property="og:type" content="company" />
-
-          <!-- Meta Pixel Code -->
-          <script>
-              !function(f,b,e,v,n,t,s)
-              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-                  n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-                  if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-                  n.queue=[];t=b.createElement(e);t.async=!0;
-                  t.src=v;s=b.getElementsByTagName(e)[0];
-                  s.parentNode.insertBefore(t,s)}(window, document,'script',
-              'https://connect.facebook.net/en_US/fbevents.js');
-              fbq('init', `${process.env.NEXT_PUBLIC_FACEBOOK_ID}`);
-              fbq('track', 'PageView');
-          </script>
-          <noscript><img height="1" width="1" style="display:none"
-                         src=`https://www.facebook.com/tr?id=${process.env.NEXT_PUBLIC_FACEBOOK_ID}&ev=PageView&noscript=1`
-          /></noscript>
-          <!-- End Meta Pixel Code -->
       </Head>
 
       <Script
