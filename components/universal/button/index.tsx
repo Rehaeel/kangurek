9d36ea4f6dfc5c74bd110e02f3bde82f69d5f747
Rafał Prokopiak <88146956+Rehaeel@users.noreply.github.com>
2023-09-19 10:31:37 +0200

    modify Btn

--- components/universal/button/index.tsx
+++ components/universal/button/index.tsx
@@ -1,23 +1,24 @@
-import { useRouter } from 'next/router';
-import { FC } from 'react';
-import { Props } from './types';
+import { useRouter } from "next/router";
+import { FC } from "react";
+import { Props } from "./types";
 
 const Button: FC<Props> = ({ children, href, onClick }) => {
-	const router = useRouter();
+  const router = useRouter();
 
-	return (
-		<button
-			onClick={() => {
-				if(onClick) onClick();
-				router.push(href)
-			}}
-			className='group py-4 px-6 bg-orange-color rounded-xl text-white text-2xl relative flex pr-6 transition-all duration-500 last:hover:pr-12'>
-			{children}
-			<p className='absolute right-4 text-4xl top-3 opacity-0 transition-all duration-500 group-hover:opacity-100'>
-				{'>'}
-			</p>
-		</button>
-	);
+  return (
+    <button
+      onClick={() => {
+        if (onClick) onClick();
+        router.push(href);
+      }}
+      className="group relative flex rounded-xl bg-orange-color py-4 px-6 pr-6 text-lg text-white transition-all duration-500 last:hover:pr-12 md:text-2xl"
+    >
+      {children}
+      <p className="absolute right-4 top-3 text-4xl opacity-0 transition-all duration-500 group-hover:opacity-100">
+        {">"}
+      </p>
+    </button>
+  );
 };
 
 export default Button;
