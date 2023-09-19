5214b3274d4060408725e3de418714753a01b01d
Rafał Prokopiak <88146956+Rehaeel@users.noreply.github.com>
2023-09-19 11:27:02 +0200

    change Modal type

--- components/universal/modal/index.tsx
+++ components/universal/modal/index.tsx
@@ -1,23 +1,24 @@
-import ReactDOM from 'react-dom';
-import { ModalProps } from './types';
+import ReactDOM from "react-dom";
+import { ModalProps } from "./types";
 
-const Modal: React.FC<ModalProps> = (props) => {
-	const { children, isOpen, setIsOpen } = props;
-	if (!isOpen) return null;
+const Modal = (props: ModalProps) => {
+  const { children, isOpen, setIsOpen } = props;
+  if (!isOpen) return null;
 
-	return ReactDOM.createPortal(
-		<div className='fixed top-0 left-0 h-screen w-screen bg-gray-900/80 z-50'>
-			<div className='relative w-full h-full flex justify-center items-center'>
-				<p
-					onClick={() => setIsOpen(false)}
-					className='absolute text-white text-2xl right-5 top-5 cursor-pointer py-2 px-4 bg-gray-700/50 rounded-lg z-20'>
-					X
-				</p>
-				{children}
-			</div>
-		</div>,
-		document.body
-	);
+  return ReactDOM.createPortal(
+    <div className="fixed top-0 left-0 z-50 h-screen w-screen bg-gray-900/80">
+      <div className="relative flex h-full w-full items-center justify-center">
+        <p
+          onClick={() => setIsOpen(false)}
+          className="absolute right-5 top-5 z-20 cursor-pointer rounded-lg bg-gray-700/50 py-2 px-4 text-2xl text-white"
+        >
+          X
+        </p>
+        {children}
+      </div>
+    </div>,
+    document.body
+  );
 };
 
 export default Modal;
