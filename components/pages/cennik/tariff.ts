819716760ff2ec048aeb5114a0e27965f3309455
Rehaeel <rafal@prokopiak.pl>
2024-02-21 12:27:38 +0100

    change terapeuci

--- components/pages/cennik/tariff.ts
+++ components/pages/cennik/tariff.ts
@@ -1,114 +1,124 @@
-import { Tariff } from './types';
+import { Tariff } from "./types";
 
 export const tariff: Tariff[] = [
-	{
-		header: 'Integracja Sensoryczna',
-		body: [
-			{
-				serviceName: 'Diagnoza zaburzeń',
-				duration: '2-3 spotkania',
-				cost: '550 zł',
-			},
-			{
-				serviceName: 'Terapia',
-				duration: '45 min.',
-				cost: '120 zł',
-			},
-		],
-	},
-	{
-		header: 'Neurologopedia',
-		body: [
-			{
-				serviceName: 'Diagnoza',
-				duration: '1 spotkanie',
-				cost: '150 zł',
-			},
-			{
-				serviceName: 'Terapia',
-				duration: '45 min',
-				cost: '120 zł',
-			},
-			{
-				serviceName: 'Terapia',
-				duration: '30 min',
-				cost: '100 zł',
-			},
-		],
-	},
-	{
-		header: 'Terapia Pedagogiczna',
-		body: [
-			{
-				serviceName: 'Indywidualna',
-				duration: '45 min',
-				cost: '110 zł',
-			},
-			{
-				serviceName: 'Grupowa',
-				duration: '45 min',
-				cost: '60 zł',
-			},
-		],
-	},
-	{
-		header: 'Terapia Ręki',
-		body: [
-			{
-				serviceName: 'Diagnoza',
-				duration: '2 spotkania',
-				cost: '400 zł',
-			},
-			{
-				serviceName: 'Terapia',
-				duration: '45 min',
-				cost: '120 zł',
-			},
-		],
-	},
-	{
-		header: 'Rehabilitacja ruchowa',
-		body: [
-			{
-				serviceName: 'Indywidualna',
-				duration: '45 min',
-				cost: '110 zł',
-			},
-		],
-	},
-	{
-		header: 'Zajęcia z psem',
-		body: [
-			{
-				serviceName: 'Indywidualne',
-				duration: '45 min',
-				cost: '100 zł',
-			},
-			{
-				serviceName: 'Grupowe',
-				duration: '45 min',
-				cost: '60 zł',
-			},
-		],
-	},
-	{
-		header: 'Dodatkowe usługi',
-		body: [
-			{
-				serviceName: 'Konsultacja',
-				duration: '1 spotkanie',
-				cost: '200 zł',
-			},
-			{
-				serviceName: 'Wydawanie dodatkowych opinii',
-				duration: '-',
-				cost: '150 zł',
-			},
-			{
-				serviceName: 'Praktyka z integracji sensorycznej',
-				duration: '2 miesiące',
-				cost: '1200 zł',
-			},
-		],
-	},
+  {
+    header: "Integracja Sensoryczna",
+    body: [
+      {
+        serviceName: "Diagnoza zaburzeń",
+        duration: "2-3 spotkania",
+        cost: "550 zł",
+      },
+      {
+        serviceName: "Terapia",
+        duration: "45 min.",
+        cost: "120 zł",
+      },
+    ],
+  },
+  {
+    header: "Neurologopedia",
+    body: [
+      {
+        serviceName: "Diagnoza",
+        duration: "1 spotkanie",
+        cost: "150 zł",
+      },
+      {
+        serviceName: "Terapia",
+        duration: "45 min",
+        cost: "120 zł",
+      },
+      {
+        serviceName: "Terapia",
+        duration: "30 min",
+        cost: "100 zł",
+      },
+    ],
+  },
+  {
+    header: "Terapia Pedagogiczna",
+    body: [
+      {
+        serviceName: "Indywidualna",
+        duration: "45 min",
+        cost: "110 zł",
+      },
+      {
+        serviceName: "Grupowa",
+        duration: "45 min",
+        cost: "60 zł",
+      },
+    ],
+  },
+  {
+    header: "Terapia Ręki",
+    body: [
+      {
+        serviceName: "Diagnoza",
+        duration: "2 spotkania",
+        cost: "400 zł",
+      },
+      {
+        serviceName: "Terapia",
+        duration: "45 min",
+        cost: "120 zł",
+      },
+    ],
+  },
+  // {
+  // 	header: 'Rehabilitacja ruchowa',
+  // 	body: [
+  // 		{
+  // 			serviceName: 'Indywidualna',
+  // 			duration: '45 min',
+  // 			cost: '110 zł',
+  // 		},
+  // 	],
+  // },
+  {
+    header: "Logopedyczna",
+    body: [
+      {
+        serviceName: "Terapia",
+        duration: "45 min",
+        cost: "100 zł",
+      },
+    ],
+  },
+  {
+    header: "Zajęcia z psem",
+    body: [
+      {
+        serviceName: "Indywidualne",
+        duration: "45 min",
+        cost: "100 zł",
+      },
+      {
+        serviceName: "Grupowe",
+        duration: "45 min",
+        cost: "60 zł",
+      },
+    ],
+  },
+  {
+    header: "Dodatkowe usługi",
+    body: [
+      {
+        serviceName: "Konsultacja",
+        duration: "1 spotkanie",
+        cost: "200 zł",
+      },
+      {
+        serviceName: "Wydawanie dodatkowych opinii",
+        duration: "-",
+        cost: "150 zł",
+      },
+      {
+        serviceName: "Praktyka z integracji sensorycznej",
+        duration: "2 miesiące",
+        cost: "1200 zł",
+      },
+    ],
+  },
 ];
--- pages/cennik.tsx
+++ pages/cennik.tsx
@@ -1,59 +1,57 @@
-import PageContent from '../components/universal/page-content';
-import Cennik from '../components/pages/cennik';
-import Button from '../components/universal/button';
-import PageWrapper from '../components/universal/page-wrapper';
-import { NextPage } from 'next';
-import Head from 'next/head';
-import { useRouter } from 'next/router';
-import { DOMAIN_NAME } from '../utils/constants';
-import { fbEvent } from '../utils/fpixel';
+"use client";
 
-const title: string = 'Cennik';
+import PageContent from "../components/universal/page-content";
+import Cennik from "../components/pages/cennik";
+import Button from "../components/universal/button";
+import PageWrapper from "../components/universal/page-wrapper";
+import { NextPage } from "next";
+import Head from "next/head";
+import { useRouter } from "next/router";
+import { DOMAIN_NAME } from "../utils/constants";
+import { fbEvent } from "../utils/fpixel";
+
+const title: string = "Cennik";
 const description: string =
-	'Sprawdź aktualne ceny zajęć, terapii i innych usług oferowanych przez Centrum Kangurek';
+  "Sprawdź aktualne ceny zajęć, terapii i innych usług oferowanych przez Centrum Kangurek";
 
 const CennikPage: NextPage = () => {
-	const router = useRouter();
+  const router = useRouter();
 
-	return (
-		<>
-			<Head>
-				<title>{title}</title>
-				<meta name='description' content={description} />
-				<meta
-					name='keywords'
-					content='cennik, integracja sensoryczna, centrum-kangurek'
-				/>
-				<meta
-					property='og:title'
-					content={`Centrum Kangurek - ${title}`}
-				/>
-				<meta property='og:description' content={description} />
-				<meta
-					property='og:url'
-					content={`${DOMAIN_NAME}/${router.pathname}`}
-				/>
-			</Head>
+  return (
+    <>
+      <Head>
+        <title>{title}</title>
+        <meta name="description" content={description} />
+        <meta
+          name="keywords"
+          content="cennik, integracja sensoryczna, centrum-kangurek"
+        />
+        <meta property="og:title" content={`Centrum Kangurek - ${title}`} />
+        <meta property="og:description" content={description} />
+        <meta property="og:url" content={`${DOMAIN_NAME}/${router.pathname}`} />
+      </Head>
 
-			<PageWrapper
-				title='cennik'
-				image='/images/pages/cennik/Cennik_zakladka_kangurek.jpg'>
-				<PageContent>
-					<Cennik />
+      <PageWrapper
+        title="cennik"
+        image="/images/pages/cennik/Cennik_zakladka_kangurek.jpg"
+      >
+        <PageContent>
+          <Cennik />
 
-					<Button
-						href='/kontakt'
-						onClick={() =>
-							fbEvent('Contact', {
-								source: 'Cennik',
-							})
-						}>
-						Zadzwoń i umów się na wizytę!
-					</Button>
-				</PageContent>
-			</PageWrapper>
-		</>
-	);
+          <Button
+            href="/kontakt"
+            onClick={() =>
+              fbEvent("Contact", {
+                source: "Cennik",
+              })
+            }
+          >
+            Zadzwoń i umów się na wizytę!
+          </Button>
+        </PageContent>
+      </PageWrapper>
+    </>
+  );
 };
 
 export default CennikPage;
