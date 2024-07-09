5fa5fbc0c399beaa2dc4eb85f7c0f2da279ba286
Rehaeel <rafal@prokopiak.pl>
2024-07-09 13:51:41 +0200

    set default HTML node for script

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,11 +1,10 @@
 import { Html, Head, Main, NextScript } from "next/document";
-import Script from "next/script";
 
 export default function Document() {
   return (
     <Html lang="pl-PL">
       <Head>
-        <Script async type="text/javascript" src="/js/newrelic.js" />
+        <script async type="text/javascript" src="/js/newrelic.js" />
       </Head>
       <body>
         <Main />
