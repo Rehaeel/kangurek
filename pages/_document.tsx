985a5091499e8b4d780bb3e35186c3b208a4573e
Rehaeel <rafal@prokopiak.pl>
2024-07-09 13:46:48 +0200

    add newrelic browser monitoring

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,9 +1,12 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
   return (
     <Html lang="pl-PL">
-      <Head />
+      <Head>
+        <Script async type="text/javascript" src="/js/newrelic.js" />
+      </Head>
       <body>
         <Main />
         <NextScript />
