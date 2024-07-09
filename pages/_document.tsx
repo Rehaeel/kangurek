4a1e71d1b18e5e5a086d284dd02bbd500ab5315d
Rehaeel <rafal@prokopiak.pl>
2024-07-09 14:16:46 +0200

    try appending the script

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,11 +1,17 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import { useEffect } from "react";
 
 export default function Document() {
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.src = "/js/newrelic.js";
+    script.async = true;
+    document.head.appendChild(script);
+  }, []);
+
   return (
     <Html lang="pl-PL">
-      <Head>
-        <script async src="/js/newrelic.js"></script>
-      </Head>
+      <Head />
       <body>
         <Main />
         <NextScript />
